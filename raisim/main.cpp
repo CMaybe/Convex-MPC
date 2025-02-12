5f155b523234d5cdaad811c51a80e169d47a2c8e
CMaybe <asbsds123@gmail.com>
2025-02-12 11:27:41 +0000

    update: first walk!

--- raisim/main.cpp
+++ raisim/main.cpp
@@ -11,7 +11,7 @@ using namespace ConvexMPC;
 int main() {
     raisim::World world;
 
-    double mpc_dt = 0.05;
+    double mpc_dt = 0.025;
     double time_step = 0.001;
     world.setTimeStep(time_step);
     auto ground = world.addGround();
@@ -30,45 +30,48 @@ int main() {
     RobotModel robot_model(inertia,
                            52.13485,
                            params::gravity,
-                           0.7,
+                           0.6,
                            mpc_dt,  // params::dt,
                            0,       // params::f_min,
-                           666);    // params::f_max
+                           700);    // params::f_max
     // params::f_max);
     Eigen::Vector3d cmd_vel;
 
     std::atomic<bool> control_execute{};
     control_execute.store(true, std::memory_order_release);
+    std::array<Eigen::Vector3d, LEG_NUM> foot_position_b, foot_velocity;
+    std::array<Eigen::Vector3d, LEG_NUM> grf, feedback, feedforward;
+    std::array<Eigen::Vector3d, LEG_NUM> grf_torque, feedback_torque, feedforward_torque;
+    std::array<Eigen::Matrix3d, LEG_NUM> foot_jacobian;
 
     Eigen::VectorXd jointNominalConfig(ANYmal->getGeneralizedCoordinateDim()), jointVelocityTarget(ANYmal->getDOF());
     Eigen::VectorXd current_jointVelocity(ANYmal->getDOF()), current_jointConfig(ANYmal->getGeneralizedCoordinateDim());
     Eigen::VectorXd jointPgain(ANYmal->getDOF()), jointDgain(ANYmal->getDOF()), jointForce(ANYmal->getDOF()),
         dynamics_torque(ANYmal->getDOF());
-    std::array<Eigen::Vector3d, LEG_NUM> foot_position_b, foot_velocity;
-    std::array<Eigen::Vector3d, LEG_NUM> grf, feedback, feedforward;
-    std::array<Eigen::Vector3d, LEG_NUM> grf_torque, feedback_torque, feedforward_torque;
     Eigen::Vector3d body_position, euler, linear_velocity, angular_velocity;
     Eigen::Vector4d quat;
     Eigen::VectorXd q_weights(13), r_weights(12);
+
+    Eigen::MatrixXd lambda;
 
     jointPgain.setZero();
     jointDgain.setZero();
     jointVelocityTarget.setZero();
     jointNominalConfig << 0, 0, 0.54, 1, 0, 0, 0, 0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8;
-    q_weights << 20, 10, 1, 0, 0, 420, 0.05, 0.05, 0.05, 30, 30, 10, 0.;
-    r_weights << 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7;
+    q_weights << 50, 10, 10, 10, 10, 100, 0.05, 0.05, 0.05, 10, 10, 10, 0.;
+    r_weights << 1e-5, 1e-5, 1e-6, 1e-5, 1e-5, 1e-6, 1e-5, 1e-5, 1e-6, 1e-5, 1e-5, 1e-6;
 
     ANYmal->setControlMode(raisim::ControlMode::PD_PLUS_FEEDFORWARD_TORQUE);
     ANYmal->setPdTarget(jointNominalConfig, jointVelocityTarget);
     ANYmal->setName("ANYmal");
 
-    ANYmal->setGeneralizedCoordinate(jointNominalConfig);
     ANYmal->setPdGains(jointPgain, jointDgain);
 
     /// launch raisim server
     raisim::RaisimServer server(&world);
     server.focusOn(ANYmal);
     server.launchServer();
+    ANYmal->setGeneralizedCoordinate(jointNominalConfig);
 
     current_jointConfig = ANYmal->getGeneralizedCoordinate().e();
     body_position = current_jointConfig.segment(0, 3);
@@ -82,17 +85,13 @@ int main() {
         foot_position_b[leg_idx] = foot_position_w.e();
     }
     RobotState robot_state(euler, body_position, angular_velocity, linear_velocity, foot_position_b);
-    RobotController robot_controller(robot_model, robot_state, q_weights, r_weights, 2000, 200);
-    robot_state.updateContactState({true, false, false, true});
-
-    ANYmal->printOutBodyNamesInOrder();
-
-    ANYmal->printOutFrameNamesInOrder();
-    std::cout << ANYmal->getDOF() << std::endl;
-    /// mpc
+    RobotController robot_controller(robot_model, robot_state, q_weights, r_weights, 700, 40, 0.2, 0.2);
+    robot_state.updateContactState({false, true, true, false});
     ANYmal->setGeneralizedCoordinate(jointNominalConfig);
+    /// mpc
     for (int i = 0; i < 10000000; i++) {
         RS_TIMED_LOOP(int(world.getTimeStep() * 1e6))
+
         current_jointConfig = ANYmal->getGeneralizedCoordinate().e();
         current_jointVelocity = ANYmal->getGeneralizedVelocity().e();
         body_position = current_jointConfig.segment(0, 3);
@@ -100,11 +99,10 @@ int main() {
         euler = utils::quaternion_to_euler(quat);
         linear_velocity = current_jointVelocity.segment(0, 3);
         angular_velocity = current_jointVelocity.segment(3, 3);
-        dynamics_torque = ANYmal->getNonlinearities({0, 0, -9.81}).e();
+        dynamics_torque = ANYmal->getNonlinearities({0, 0, params::gravity}).e();
         robot_state.updateState(euler, body_position, angular_velocity, linear_velocity);
 
-        std::array<Eigen::Matrix3d, LEG_NUM> foot_jacobian;
-        cmd_vel = Eigen::Vector3d(0, 0, 0);
+        cmd_vel = {1.0, 0, 0};
         for (int leg_idx = 0; leg_idx < LEG_NUM; leg_idx++) {
             raisim::Vec<3> foot_position_w;
             Eigen::MatrixXd full_jacobian(3, ANYmal->getDOF());
@@ -122,7 +120,7 @@ int main() {
         }
         feedback = robot_controller.computeSwingForce(robot_state, cmd_vel);
         for (int leg_idx = 0; leg_idx < LEG_NUM; leg_idx++) {
-            grf_torque[leg_idx] = -foot_jacobian[leg_idx].transpose() * grf[leg_idx];
+            grf_torque[leg_idx] = -foot_jacobian[leg_idx].transpose() * 1.2 * grf[leg_idx];
             feedforward[leg_idx] = dynamics_torque.segment(6 + 3 * leg_idx, 3);
             feedback_torque[leg_idx] = foot_jacobian[leg_idx].transpose() * feedback[leg_idx];
         }
